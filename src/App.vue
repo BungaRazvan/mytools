9b538fc39c67437ccadcd9aecf758d55b9db3603
Bunga Razvan <razvanbunga@gmail.com>
2023-03-25 16:12:43 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard display-animation">
+    <WindowsTile />
+  </div>
+</template>
+
+<script>
+import "./assets/scss/style.scss";
+
+import WindowsTile from "./components/WindowsTile.vue";
+
+export default {
+  name: "App",
+  components: { WindowsTile },
+};
+</script>
---
+++ src/components/WindowsTile.vue
@@ -0,0 +1,18 @@
+<template>
+  <a class="tile tile-lg tile-pink ripple-effect" href="#">
+    <span class="content-wrapper">
+      <span class="tile-content">
+        <span class="tile-img"></span>
+        <span class="tile-holder tile-holder-sm">
+          <span class="title">Google Guidelines</span>
+        </span>
+      </span>
+    </span>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "WindowsTile",
+};
+</script>
