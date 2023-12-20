5d8467021b49c9c8eeb084d8b3c073040854444d
Bunga Razvan <razvanbunga@gmail.com>
2023-12-20 19:05:32 +0000

    MT-27: Relic Scorer.

---
+++ src/lib/python/scorer/algo.py
@@ -0,0 +1,22 @@
+class Character:
+    def __init__():
+        pass
+
+    def character_weigths():
+        raise NotImplemented
+
+    def parts_weight():
+        raise NotImplemented
+
+    def suitable_relic():
+        raise NotImplemented
+
+
+class Kafka(Character):
+    def __init__():
+        pass
+
+    def parts_weight():
+        return {
+            
+        }
